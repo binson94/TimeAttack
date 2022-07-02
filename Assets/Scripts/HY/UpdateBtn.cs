96d95addd349220e14dd3e262874e6a574c6c513
Hayoung <104951924+Hayoungaaaa@users.noreply.github.com>
2022-07-03 00:06:27 +0900

    [시작화면]

---
+++ Assets/Scripts/HY/UpdateBtn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateBtn : MonoBehaviour
+{
+    public void UpgradeBtn(int idx)
+    {
+
+    }
+}
